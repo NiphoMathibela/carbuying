e114f138c8eb97995b461b40c81580c46b68446e
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-06-27 13:50:28 +0200

    CarCard added

--- client/src/components/HomeContent.jsx
+++ client/src/components/HomeContent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Income from './Income'
 import PopularCarsCard from './PopularCarsCard'
+import CarCard from './CarCard'
 
 const HomeContent = () => {
   return (
     <div>
         <Income/>
         <PopularCarsCard car="Polo" gearType="Manual" consumption="5.2" seats="5" price="450,000" brand="Volkswagen" img="https://github.com/NiphoMathibela/carbuying/blob/main/client/src/assets/Polo%20Trendline.png?raw=true"/>
+        <CarCard car="Polo" gearType="Manual" consumption="5.2" seats="5" price="450,000" brand="Volkswagen" img="https://github.com/NiphoMathibela/carbuying/blob/main/client/src/assets/Polo%20Trendline.png?raw=true"/>
     </div>
   )
 }
