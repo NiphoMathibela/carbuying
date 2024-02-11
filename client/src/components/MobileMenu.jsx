63100bc70833b4565ce6ca3497daee7134df6c9f
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-02-11 19:33:57 +0200

    Fixed mobile menu

--- client/src/components/MobileMenu.jsx
+++ client/src/components/MobileMenu.jsx
@@ -21,10 +21,11 @@ const MobileMenu = () => {
     }
 
     return (
-        <div className='w-full h-screen fixed mx-auto z-40 top-12 bg-[#F6F7F9] bg-opacity-80 sm:hidden'>
+        <div className='w-full h-screen fixed mx-auto z-40 top-12 bg-[#F6F7F9] bg-opacity-85 sm:hidden'>
             <div className='text-[#3563E9] text-lg font-medium'>
-                <li className='my-4 cursor-pointer' onClick={NavigateSellCar} >Sell car</li>
-                <li className='my-4 cursor-pointer' onClick={NavigateLogin}>Login</li>
+                <li className='mt-4 mb-2 cursor-pointer list-none w-fit mx-auto active:bg-[#3563E9] active:text-white p-2 rounded-md' onClick={NavigateSellCar} >Sell a car</li>
+                <li className='my-2 cursor-pointer list-none w-fit mx-auto active:bg-[#3563E9] active:text-white p-2 rounded-md' onClick={NavigateLogin}>Login</li>
+                <li className='my-2 cursor-pointer list-none w-fit mx-auto active:bg-[#3563E9] active:text-white p-2 rounded-md' onClick={NavigateLogin}>Sign out</li>
             </div>
         </div>
     )
